fa90a9738b72160d0c88214d036e3d1db9539a78
Bhagyalaxmi05 <147228932+Bhagyalaxmi05@users.noreply.github.com>
2026-02-17 15:33:53 +0530

    Add files via upload

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas as pd 
-import numpy as np
+# import pandas as pd 
+# import numpy as np
 
 # st.title("Hello, Streamlit !")
 # st.write("This is simple Streamlit app.")
